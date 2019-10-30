00f9eab1154ca0b073a4e45ec95f266b6367b1e2
Scott Sum <44307826+scott-sum@users.noreply.github.com>
2019-10-29 21:37:35 -0400

    third push

---
+++ Lotto/Lotto.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Lotto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnPick = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnPick
+            // 
+            this.BtnPick.AutoSize = true;
+            this.BtnPick.Location = new System.Drawing.Point(237, 92);
+            this.BtnPick.Name = "BtnPick";
+            this.BtnPick.Size = new System.Drawing.Size(165, 29);
+            this.BtnPick.TabIndex = 0;
+            this.BtnPick.Text = "Get My Lucky Numbers";
+            this.BtnPick.UseVisualStyleBackColor = true;
+            this.BtnPick.Click += new System.EventHandler(this.BtnPick_Click);
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Location = new System.Drawing.Point(481, 92);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(87, 29);
+            this.BtnReset.TabIndex = 1;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(620, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(544, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(468, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(392, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 32);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(316, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 32);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(240, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 32);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Lotto.Properties.Resources.Webp_net_resizeimage;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 177);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.BtnPick);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lotto Number Picker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnPick;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
